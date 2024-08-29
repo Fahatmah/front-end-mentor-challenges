cfdb7bbe84d7050d11f2dd24427aeb3eb46e84d7
Fahatmah <fahatmahmabang9@gmail.com>
2024-08-29 12:51:57 +0800

    fix(billing): annual calculation, update text refactor(button): move function to separate callback
    - Reflect yearly billing
    - Maintainability

--- interactive-pricing-component/script.js
+++ interactive-pricing-component/script.js
@@ -8,7 +8,7 @@ let toggleButton = document.getElementById('toggleBill')
 const values = [8, 12, 16, 24, 36]
 const pageViews = [10, 50, 100, 500, 1]
 
-priceRangeSlider.addEventListener('input', () => {
+const updatePageViewsAndPrice = () => {
   // set a dynamic width for the progress bar
   const maxValue = priceRangeSlider.getAttribute('max')
   const value = (priceRangeSlider.value / maxValue) * 100 + '%'
@@ -27,23 +27,30 @@ priceRangeSlider.addEventListener('input', () => {
       ? discount(selectedPrice)
       : selectedPrice
   }.00`
-})
+}
 
-toggleButton.addEventListener('click', (e) => {
+const setBillingMode = (e) => {
   let price = Number(priceEl.textContent.replace(/^\$/, '').slice(0, 2))
 
   e.currentTarget.classList.toggle('toggle-yearly')
 
-  if (toggleButton.classList.contains('toggle-yearly'))
+  if (toggleButton.classList.contains('toggle-yearly')) {
     priceEl.textContent = `$${discount(price)}.00`
-  else priceEl.textContent = `$${values[priceRangeSlider.value]}.00`
-})
+    dateEl.textContent = '/ year'
+  } else {
+    priceEl.textContent = `$${values[priceRangeSlider.value]}.00`
+    dateEl.textContent = '/ month'
+  }
+}
 
 const discount = (price) => {
   const annualPrice = price * 12
   const discountedPrice = annualPrice * 0.75
-  return discountedPrice / 12
+  return discountedPrice
 }
+
+priceRangeSlider.addEventListener('input', updatePageViewsAndPrice)
+toggleButton.addEventListener('click', setBillingMode)
 
 // - 10K page views / $8 per month
 // - 50K page views / $12 per month
@@ -52,3 +59,8 @@ const discount = (price) => {
 // - 1M page views / $36 per month
 
 // Yearly Billing 25% discount
+
+// FIX
+// toggling yearly billing should
+// show an annual payment depending
+// on number of page views
