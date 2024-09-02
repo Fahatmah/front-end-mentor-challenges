7a04f5517ca6bbf2fa2fe3b6170e7402b7d5bf34
Fahatmah <fahatmahmabang9@gmail.com>
2024-09-03 06:00:55 +0800

    feat(cart): add localStorage handling, update UI for cart items

--- product-list-with-cart/script.js
+++ product-list-with-cart/script.js
@@ -1,5 +1,4 @@
 const productListContainer = document.querySelector('.products-list')
-let count = 1
 
 fetch('./data.json')
   .then((res) => {
@@ -15,9 +14,15 @@ fetch('./data.json')
   })
 
 function displayProducts(products) {
-  products.map((product) => {
+  const cart = getItemsLocalStorage()
+
+  products.map((product, i) => {
     let article = document.createElement('article')
     article.classList.add('product-item', 'fl-c')
+
+    // checking if item already exist in the cart
+    const cartItem = cart.find((item) => item.itemId === String(i + 1))
+
     articleContent = `
       <div class="product-image">
         <picture class="fl-c">
@@ -42,13 +47,57 @@ function displayProducts(products) {
         <div class="product-buttons">
           <button id="addToCart" class="fl-c" onclick="addToCart(event)" data-name="${
             product.name
-          }">
+          }" data-item-id="${i + 1}" style="${
+      cartItem ? 'visibility: hidden' : ''
+    }">
           <img
             src="./assets/images/icon-add-to-cart.svg"
             alt="icon-add-to-cart"
           />
           Add to Cart
             </button>
+            ${
+              cartItem
+                ? `
+                <div class="quantity-control-buttons fl-c">
+                  <button id="decrementQuantity" class="fl-c" onclick="decrementItem(event)" data-name="${
+                    product.name
+                  }" data-item-id="${i + 1}">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="2"
+                        fill="none"
+                        viewBox="0 0 10 2"
+                      >
+                        <path fill="#fff" d="M0 .375h10v1.25H0V.375Z" />
+                      </svg>
+                    </button>
+
+                    <p class="product-quantity" data-quantity="${
+                      cartItem.count
+                    }">${cartItem.count}</p>
+
+                    <button id="incrementQuantity" class="fl-c" onclick="incrementItem(event)" data-name="${
+                      product.name
+                    }" data-item-id="${i + 1}">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="10"
+                        height="10"
+                        fill="none"
+                        viewBox="0 0 10 10"
+                      >
+                        <path
+                          fill="#fff"
+                          d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>`
+                : ''
+            }
           </div>
         </div>
 
@@ -70,12 +119,12 @@ function addToCart(e) {
   pictureContainer.classList.add('active')
 
   const productName = e.currentTarget.getAttribute('data-name')
-  console.log(productName)
+  const productID = e.currentTarget.getAttribute('data-item-id')
 
   const quantityBtnsContainer = document.createElement('div')
   quantityBtnsContainer.classList.add('quantity-control-buttons', 'fl-c')
   quantityBtnsContainer.innerHTML = `
-                <button id="decrementQuantity" class="fl-c" onclick="decrementItem(event)" data-name="${productName}">
+                <button id="decrementQuantity" class="fl-c" onclick="decrementItem(event)" data-name="${productName}" data-item-id="${productID}">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="10"
@@ -89,7 +138,7 @@ function addToCart(e) {
 
                   <p class="product-quantity" data-quantity="1">1</p>
 
-                  <button id="incrementQuantity" class="fl-c" onclick="incrementItem(event)" data-name="${productName}">
+                  <button id="incrementQuantity" class="fl-c" onclick="incrementItem(event)" data-name="${productName}" data-item-id="${productID}">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="10"
@@ -110,20 +159,31 @@ function addToCart(e) {
     parent.insertBefore(quantityBtnsContainer, parent.lastChild)
     e.currentTarget.style.visibility = 'hidden'
   }
+
+  addItemLocalStorage({ itemId: productID, item: productName, count: 1 })
 }
 
 function incrementItem(e) {
   const productName = e.currentTarget.getAttribute('data-name')
+  const productID = e.currentTarget.getAttribute('data-item-id')
+
   const itemCount = e.currentTarget.previousElementSibling
   let currentQuantity = parseInt(itemCount.getAttribute('data-quantity'))
 
   currentQuantity++
   itemCount.textContent = currentQuantity
   itemCount.setAttribute('data-quantity', currentQuantity)
+
+  updateItemLocalStorage({
+    itemId: productID,
+    item: productName,
+    count: currentQuantity,
+  })
 }
 
 function decrementItem(e) {
   const productName = e.currentTarget.getAttribute('data-name')
+  const productID = e.currentTarget.getAttribute('data-item-id')
   const itemCount = e.currentTarget.nextElementSibling
   let currentQuantity = parseInt(itemCount.getAttribute('data-quantity'))
   currentQuantity--
@@ -138,5 +198,43 @@ function decrementItem(e) {
     addToCartBtn.style.visibility = 'visible'
     e.currentTarget.parentElement.remove()
     currentQuantity = 0
+
+    removeItemLocalStorage(productID)
+  } else
+    updateItemLocalStorage({
+      itemId: productID,
+      item: productName,
+      count: currentQuantity,
+    })
+}
+
+function setItemLocalStorage(cart) {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
+
+function getItemsLocalStorage() {
+  return JSON.parse(localStorage.getItem('cart')) || []
+}
+
+function addItemLocalStorage(item) {
+  let cart = getItemsLocalStorage()
+  cart.push(item)
+  setItemLocalStorage(cart)
+}
+
+function updateItemLocalStorage(updateItem) {
+  let cart = getItemsLocalStorage()
+  let index = cart.findIndex((item) => item.itemId === updateItem.itemId)
+  console.log(index)
+
+  if (index !== -1) {
+    cart[index] = updateItem
+    setItemLocalStorage(cart)
   }
+}
+
+function removeItemLocalStorage(productId) {
+  let cart = getItemsLocalStorage()
+  cart = cart.filter((item) => item.itemId !== productId)
+  setItemLocalStorage(cart)
 }
